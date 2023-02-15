5539f1e0daab93bc3bf706dbfe875f83915e24e9
Florian Latapie <florian.latapie@etu.unice.fr>
2023-02-15 18:42:34 +0100

    Update hello_ucosii.c

--- software/TP2/hello_ucosii.c
+++ software/TP2/hello_ucosii.c
@@ -454,24 +454,24 @@ void task4(void* pdata){
 
 		OSTaskStkChk(1, &p);
 		printf("Task 1 Free: %lu\n", p.OSFree);
-		printf("Task 1 Used: %lu\n\n", p.OSUsed);
-		printf("TaskCtr: %d \n",tasks[0].TaskCtr);
-		printf("Task exec time: %d ms\n",tasks[0].TaskExecTime);
-		printf("Total exec time: %d ms\n",tasks[0].TaskTotExecTime);
+		printf("Task 1 Used: %lu\n", p.OSUsed);
+		printf("Task 1 TaskCtr: %d \n",tasks[0].TaskCtr);
+		printf("Task 1 Task exec time: %d ms\n",tasks[0].TaskExecTime);
+		printf("Task 1 Total exec time: %d ms\n\n",tasks[0].TaskTotExecTime);
 
 		OSTaskStkChk(2, &p);
 		printf("Task 2 Free: %lu\n", p.OSFree);
-		printf("Task 2 Used: %lu\n\n", p.OSUsed);
-		printf("TaskCtr: %d \n",tasks[1].TaskCtr);
-		printf("Task exec time: %d ms\n",tasks[1].TaskExecTime);
-		printf("Total exec time: %d ms\n",tasks[1].TaskTotExecTime);
+		printf("Task 2 Used: %lu\n", p.OSUsed);
+		printf("Task 2 TaskCtr: %d \n",tasks[1].TaskCtr);
+		printf("Task 2 Task exec time: %d ms\n",tasks[1].TaskExecTime);
+		printf("Task 2 Total exec time: %d ms\n\n",tasks[1].TaskTotExecTime);
 
 		OSTaskStkChk(3, &p);
 		printf("Task 3 Free: %lu\n", p.OSFree);
-		printf("Task 3 Used: %lu\n\n", p.OSUsed);
-		printf("TaskCtr: %d \n",tasks[2].TaskCtr);
-		printf("Task exec time: %d ms\n",tasks[2].TaskExecTime);
-		printf("Total exec time: %d ms\n",tasks[2].TaskTotExecTime);
+		printf("Task 3 Used: %lu\n", p.OSUsed);
+		printf("Task 3 TaskCtr: %d \n",tasks[2].TaskCtr);
+		printf("Task 3 Task exec time: %d ms\n",tasks[2].TaskExecTime);
+		printf("Task 3 Total exec time: %d ms\n\n",tasks[2].TaskTotExecTime);
 
 		OSMboxPost(mailBox1_4, (void *)message);
 
