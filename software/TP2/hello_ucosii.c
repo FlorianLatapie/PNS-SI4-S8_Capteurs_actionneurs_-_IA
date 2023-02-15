97429595ee8d1c471dec4404d92ee86be978e03c
Vinh <narutovinh@hotmail.fr>
2023-02-15 18:49:28 +0100

    .h

--- software/TP2/hello_ucosii.c
+++ software/TP2/hello_ucosii.c
@@ -31,7 +31,7 @@
 #include <stdio.h>
 #include "includes.h"
 #include <unistd.h>
-#include <altera_avalon_pio_regs.h>
+#include "altera_avalon_pio_regs.h"
 #include "altera_avalon_timer_regs.h"
 #include "system.h"
 #include <stdlib.h>
