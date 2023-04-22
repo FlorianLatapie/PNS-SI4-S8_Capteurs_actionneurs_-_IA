0b6b3a58b086bb162cb5e420472a6e8d4410ef35
Florian Latapie <florian.latapie@etu.unice.fr>
2023-04-22 16:45:09 +0200

    ratio

--- TP_IA/Projet/create_training_list.py
+++ TP_IA/Projet/create_training_list.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 # create train, test and text pointers
 recordings_folder = "recordings"
@@ -21,11 +22,18 @@ with open(os.path.join(recordings_folder, file_name), "w", encoding="utf-8") as 
                     if os.path.isfile(sound_filename) and "_of_" in sound_filename:
                         f.write(sound_filename + "\n")
                         count += 1
-    number_of_main_bird_recordings = count / 2
-    for index, recording in enumerate(os.listdir(os.path.join(recordings_folder, main_bird.replace(" ", "_")))):
-        if index > number_of_main_bird_recordings:
-            break
-        f.write(os.path.join(recordings_folder, main_bird.replace(" ", "_"), recording) + "\n")
 
+    ratio = count / len(os.listdir(os.path.join(recordings_folder, main_bird.replace(" ", "_"))))
+    number_of_main_bird_recordings = count / 2
+    count = 0
+
+    for recording in os.listdir(os.path.join(recordings_folder, main_bird.replace(" ", "_"))):
+        if count > number_of_main_bird_recordings:
+            break
+
+        # if random number between 0 and 1 is greater than 0.3 then add to testing_list.txt
+        if np.random.rand() > ratio:
+            count += 1
+            f.write(os.path.join(recordings_folder, main_bird.replace(" ", "_"), recording) + "\n")
 
 print("Done")
