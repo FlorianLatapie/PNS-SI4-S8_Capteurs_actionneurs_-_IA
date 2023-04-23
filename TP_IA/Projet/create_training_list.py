97bf1a37167e5bd6afeee63a0cd255ab2db76b5c
Vinh <narutovinh@hotmail.fr>
2023-04-23 11:54:15 +0200

    ça commence

--- TP_IA/Projet/create_training_list.py
+++ TP_IA/Projet/create_training_list.py
@@ -10,16 +10,17 @@ file_name = "testing_list.txt"
 count = 0
 with open(os.path.join(recordings_folder, file_name), "w", encoding="utf-8") as f:
     for bird_type in os.listdir(recordings_folder):
-        if bird_type != main_bird.replace(" ", "_"):
-            bird_folder = os.path.join(recordings_folder, bird_type)
-            if os.path.isdir(bird_folder):
-                for recording in os.listdir(bird_folder):
-                    sound_filename = os.path.join(bird_folder, recording)
-                    if os.path.isfile(sound_filename) and "splitted_" in sound_filename:
+        bird_folder = os.path.join(recordings_folder, bird_type)
+        if os.path.isdir(bird_folder):
+            for recording in os.listdir(bird_folder):
+                sound_filename = os.path.join(bird_folder, recording)
+                if os.path.isfile(sound_filename) and "splitted_" in sound_filename:
+                    if np.random.rand() > 0.3:
                         f.write(sound_filename + "\n")
                         count += 1
     ratio = count / len(os.listdir(os.path.join(recordings_folder, main_bird.replace(" ", "_"))))
-    number_of_main_bird_recordings = count / (sum(os.path.isdir(os.path.join(recordings_folder, f)) for f in os.listdir(recordings_folder)) - 1)
+    number_of_main_bird_recordings = count / (
+                sum(os.path.isdir(os.path.join(recordings_folder, f)) for f in os.listdir(recordings_folder)) - 1)
 
     count = 0
 
